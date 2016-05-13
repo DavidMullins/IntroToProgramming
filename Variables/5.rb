5a896ba68f67ec346b0fe2848778ff3b4cc948a8
David Mullins <mullinsdavid88@gmail.com>
2016-05-12 21:16:51 -0400

    Adding chapter 'Variables'

---
+++ Variables/5.rb
@@ -0,0 +1,3 @@
+# The first case will return 3 for the value of x.
+# The second case will trigger an error because x is initialized in the scope of the do block.
+# Therefore x is not accessable outside of that scope.
---
+++ Variables/6.rb
@@ -0,0 +1,5 @@
+# This error tells me the following:
+# This error likely occurs on line 3 and/or line 16.
+# The program is trying to reference a variable named shoes or call to
+# a method named shoes that is either never defined in the program or 
+# defined in a scope out of the programs reach at this point in time.

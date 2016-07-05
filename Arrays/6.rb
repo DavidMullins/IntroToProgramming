2f78be5208396f5cf2229f98aa4232e5c42d6cda
David Mullins <mullinsdavid88@gmail.com>
2016-07-05 19:44:52 -0400

    Arrays chapter addded

---
+++ Arrays/6.rb
@@ -0,0 +1,3 @@
+# The issue is that Array[x] expects an integer for the value of x. You have attempted to pass
+# a string as that value. To accomplish your goal of changing Margaret to Jody, do the following:
+# names[3] = 'Jody'
---
+++ Arrays/7.rb
@@ -0,0 +1,10 @@
+array = [1,2,3,4]
+new_array = []
+
+array.each do |x|
+  new_array << x + 2
+end
+
+p array
+
+p new_array
